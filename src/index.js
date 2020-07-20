8d40fec5e7d96bcce5a909c13e773f93dc88ff37
sollama <40960146+sollama@users.noreply.github.com>
2020-07-20 12:37:34 -0700

    junk commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import App from './components/Home';
+import Header from './components/Header';
+import Generator from './projects/generator';
+
+
+ReactDOM.render(
+    <div>
+        <Router history={createBrowserHistory()}>
+            <Switch>
+                <Route exact path={process.env.PUBLIC_URL + '/'} render={() => <Header><App /></Header>} />
+                <Route path='/generator' render={() => <Header><Generator /></Header>} />
+            </Switch>
+        </Router>
+    </div>,
+document.getElementById('root'));
+
