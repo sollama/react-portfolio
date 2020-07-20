68dfdd2964e18b08ad242dc8d18f8314e836b4bd
sollama <40960146+sollama@users.noreply.github.com>
2020-07-20 13:47:43 -0700

    Updates

---
+++ precache-manifest.686a6ebc0771abf1c2f76ae9506b2b46.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5015e1861ea2ca2eea9fbcfbe99a90",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d2da872af0aa929357d6",
+    "url": "/react-portfolio/static/css/main.29ffff19.chunk.css"
+  },
+  {
+    "revision": "f5382870d36285266f26",
+    "url": "/react-portfolio/static/js/2.814e190a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.814e190a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2da872af0aa929357d6",
+    "url": "/react-portfolio/static/js/main.eccdadae.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "900596d7f6ba98ca47fcb9d108585ff8",
+    "url": "/react-portfolio/static/media/atomicantmodels.900596d7.png"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/react-portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "5524dc1a077103c7ecb29fc5a0383e94",
+    "url": "/react-portfolio/static/media/fontanaplaza.5524dc1a.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/react-portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/react-portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "58c7cbb9a02fcc8dd38cf95c8e344258",
+    "url": "/react-portfolio/static/media/mangotreesupermarket.58c7cbb9.png"
+  },
+  {
+    "revision": "bfcd2dd1d8fdc4f629741da810684d18",
+    "url": "/react-portfolio/static/media/pokeman.bfcd2dd1.png"
+  },
+  {
+    "revision": "c7a8266e262677618fb1ed9f122f610f",
+    "url": "/react-portfolio/static/media/profile.c7a8266e.JPG"
+  },
+  {
+    "revision": "9a2931180d6b1dc7b33052657eef554b",
+    "url": "/react-portfolio/static/media/segoe_ui.9a293118.woff"
+  },
+  {
+    "revision": "52382539737f4e9913e4bf6b9966bee3",
+    "url": "/react-portfolio/static/media/segoe_ui_bold.52382539.woff"
+  },
+  {
+    "revision": "a46b32a188f62e9c79d1753374daf407",
+    "url": "/react-portfolio/static/media/thebridgesconnection.a46b32a1.png"
+  },
+  {
+    "revision": "63b8b407de3232cf38a72f0d2d260111",
+    "url": "/react-portfolio/static/media/twitterism.63b8b407.png"
+  }
+]);
